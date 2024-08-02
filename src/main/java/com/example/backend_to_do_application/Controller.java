fddca5d06e8c7685499232baf3d0f3e294f1de5f
whgkswo <gjuk7238@gmail.com>
2024-08-02 15:07:44 +0900

    fourth

--- src/main/java/com/example/backend_to_do_application/Controller.java
+++ src/main/java/com/example/backend_to_do_application/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     public String welcome (){
-        return "Backend To-do Application!";
+        return "Backend To-do Application! 2";
     }
 }
