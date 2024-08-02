8ed14d2a55d6f4b861e68b92d0382e47481491c0
whgkswo <gjuk7238@gmail.com>
2024-08-02 14:55:23 +0900

    initial commit

---
+++ src/main/java/com/example/backend_to_do_application/WhgkswoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_to_do_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhgkswoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhgkswoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_to_do_application/WhgkswoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_to_do_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhgkswoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
